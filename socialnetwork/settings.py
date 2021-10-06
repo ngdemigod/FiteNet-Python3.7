0cc72a459d8c233e843baa24e5299ff847f02129
ngdemigod <augustinesaleh@gmail.com>
2021-10-06 15:06:45 -0400

    changed back to db.sqlite3

--- socialnetwork/settings.py
+++ socialnetwork/settings.py
@@ -98,8 +98,8 @@ WSGI_APPLICATION = 'socialnetwork.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'ciba',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
